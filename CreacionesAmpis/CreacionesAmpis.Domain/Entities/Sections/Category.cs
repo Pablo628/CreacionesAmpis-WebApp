d5053b6def56b67a7220639a023116cab90fc76f
Christopherjim90 <christopher330387@correo.itm.edu.co>
2026-04-15 15:47:13 -0500

    Second Entity: Four commit

--- CreacionesAmpis/CreacionesAmpis.Domain/Entities/Sections/Category.cs
+++ CreacionesAmpis/CreacionesAmpis.Domain/Entities/Sections/Category.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CreacionesAmpis.Domain.Exceptions.PrivateBlog.Domain.Exceptions;
 
 namespace CreacionesAmpis.Domain.Entities.Sections
 {
-    internal class Category
+    public class Category
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; private set; } = new List<Product>();
+
+        public Category(string name)
+        {
+            SetName(name);
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new DomainException("Category name cannot be empty");
+
+            if (name.Length > 50)
+                throw new DomainException("Category name cannot exceed 50 characters");
+
+            Name = name;
+        }
     }
 }
---
+++ CreacionesAmpis/CreacionesAmpis.Domain/Entities/Sections/Product.cs
@@ -0,0 +1,59 @@
+﻿using CreacionesAmpis.Domain.Exceptions.PrivateBlog.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreacionesAmpis.Domain.Entities.Sections
+{
+    public class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public bool IsCustomizable { get; private set; }
+
+        public int CategoryId { get; private set; }
+        public Category Category { get; private set; }
+
+        public Product(string name, string description, decimal price, int categoryId)
+        {
+            SetName(name);
+            SetDescription(description);
+            SetPrice(price);
+            CategoryId = categoryId;
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new DomainException("Product name cannot be empty");
+
+            if (name.Length > 100)
+                throw new DomainException("Product name cannot exceed 100 characters");
+
+            Name = name;
+        }
+
+        public void SetDescription(string description)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+                throw new DomainException("Description cannot be empty");
+
+            Description = description;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            if (price <= 0)
+                throw new DomainException("Price must be greater than zero");
+
+            Price = price;
+        }
+
+        public void SetCustomizable(bool value)
+        {
+            IsCustomizable = value;
+        }
+    }
+}
