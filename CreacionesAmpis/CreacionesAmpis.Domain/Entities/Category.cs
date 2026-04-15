bdf76a1f259c1600378f482cdaf9e3a3a4aa1303
Christopherjim90 <christopher330387@correo.itm.edu.co>
2026-04-15 15:40:17 -0500

    Initial Entity:Three commit

--- CreacionesAmpis/CreacionesAmpis.Domain/Entities/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CreacionesAmpis.Domain.Entities;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = default!;
-    public ICollection<Product> Products { get; set; } = new List<Product>();
-}
---
+++ CreacionesAmpis/CreacionesAmpis.Domain/Entities/Sections/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreacionesAmpis.Domain.Entities.Sections
+{
+    internal class Category
+    {
+    }
+}
